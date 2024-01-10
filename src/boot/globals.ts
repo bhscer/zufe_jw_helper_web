ecf686bf4a88268784b8c633c9562b794949814d
bhscer <129813414+bhscer@users.noreply.github.com>
2024-01-10 20:17:40 +0800

    feat: 引入Vercel Speed Insights

--- src/boot/globals.ts
+++ src/boot/globals.ts
@@ -1,8 +1,10 @@
 import { boot } from 'quasar/wrappers';
 
+import { inject } from '@vercel/analytics';
 import 'viewerjs/dist/viewer.css';
 import VueViewer from 'v-viewer';
 
 export default boot(async ({ app }) => {
+  inject();
   app.use(VueViewer);
 });
--- src/boot/vercel_analytics.ts
+++
@@ -1,7 +0,0 @@
-import { boot } from 'quasar/wrappers';
-
-import { inject } from '@vercel/analytics';
-
-export default boot(async ({ app }) => {
-  inject();
-});
