025740c261e9cbc753e6fa804cdc07476fe785ee
bhscer <129813414+bhscer@users.noreply.github.com>
2024-01-28 23:16:32 +0800

    fix: sentry作用域问题

--- config.js
+++ config.js
@@ -1,9 +1,11 @@
 module.exports = {
   dev: {
+    prod: false,
     BASE_URL: 'http://localhost:8080/',
     // BASE_URL: 'https://jw-api.bhscer.com/',
   },
   prod: {
+    prod: true,
     BASE_URL: 'https://jw-api.bhscer.com/',
     // BASE_URL: 'http://172.31.140.189:8011', // special version for vm
   },
--- src/boot/globals.ts
+++ src/boot/globals.ts
@@ -13,30 +13,31 @@ export default boot(async ({ app }) => {
     },
   });
 
-  Sentry.init({
-    app,
-    dsn: 'https://9a3b8c2a7043f7a0bfe9d3ffe85fd980@o4506649028526080.ingest.sentry.io/4506649030557696',
-    integrations: [
-      new Sentry.BrowserTracing({
-        // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-        tracePropagationTargets: [
-          // 'localhost',
-          'jw.bhscer.com',
-          'bhscer.gitee.io',
-        ],
-      }),
-      new Sentry.Replay({
-        maskAllText: false,
-        blockAllMedia: false,
-      }),
-    ],
-    // Performance Monitoring
-    tracesSampleRate: 1.0, //  Capture 100% of the transactions
-    // Session Replay
-    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
-  });
-
+  if (process.env.prod) {
+    Sentry.init({
+      app,
+      dsn: 'https://9a3b8c2a7043f7a0bfe9d3ffe85fd980@o4506649028526080.ingest.sentry.io/4506649030557696',
+      integrations: [
+        new Sentry.BrowserTracing({
+          // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+          // tracePropagationTargets: [
+          //   // 'localhost',
+          //   'jw.bhscer.com',
+          //   'bhscer.gitee.io',
+          // ],
+        }),
+        new Sentry.Replay({
+          maskAllText: false,
+          blockAllMedia: false,
+        }),
+      ],
+      // Performance Monitoring
+      tracesSampleRate: 1.0, //  Capture 100% of the transactions
+      // Session Replay
+      replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+      replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+    });
+  }
   app.config.globalProperties.$TimestampToDate = (Timestamp: number) => {
     const now = new Date(Timestamp * 1000),
       y = now.getFullYear(),
